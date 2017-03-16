8563c57840fa77738d7253593663d772d51e4148
piaoliu <441594700@qq.com>
2017-03-16 20:59:09 +0800

    some modify

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # data_storage
 Initialize the database, import data from excel to MySQL, PostgreSQL, Oracle
+
+## main lib
+- pandas
+- toml
+- logbook
+
+## main function
+1. Recursively search the specified path, find excel files and csv files
+
+2. Recursively traverse all the sheets of excel and return the DataFrame. If there is a null value on the first line, it will drop it and crawl the second line is the table field, and so on.
+
+3. Now tested, it can import Mysql/Postgresql/Oracle database. Thanks for all the pandas developers.
+
+4. Take the sheet name as the table name.
+
+5. But at some point, pandas to excel data to identify the error, or their own data problems, will be an error, such as timestamp object without translate attributes.
--- lib/data_storage.py
+++ lib/data_storage.py
@@ -3,22 +3,25 @@ from sqlalchemy import create_engine
 import pymysql
 from logbook import Logger
 
-
 log = Logger('DataStorage')
+
 
 class DataStorageBase:
     def __init__(self, database, user, password, port, db, host='localhost'):
         if database == 'mysql':
-            self.engine = create_engine('{0}+pymysql://{1}:{2}@{3}:{4}/{5}'.format(database, user, password, host, port, db))
+            self.engine = create_engine(
+                '{0}+pymysql://{1}:{2}@{3}:{4}/{5}'.format(database, user, password, host, port, db))
         else:
             self.engine = create_engine('{0}://{1}:{2}@{3}:{4}/{5}'.format(database, user, password, host, port, db))
 
     def send(self, tablename, data):
         data.to_sql(tablename, con=self.engine, chunksize=1000, if_exists='append')
 
+
 class ExcelDataStorage(DataStorageBase):
     def __init__(self, conf, datas):
-        DataStorageBase.__init__(self, conf['database'].lower(), conf['user'], conf['password'], conf['port'], conf['db'],
+        DataStorageBase.__init__(self, conf['database'].lower(), conf['user'], conf['password'], conf['port'],
+                                 conf['db'],
                                  conf['host'])
         self.datas = datas
         self.conf = conf
@@ -30,12 +33,19 @@ class ExcelDataStorage(DataStorageBase):
                 self.analysis(file)
                 for tablename, tabledata in self.data:
                     self.send(tablename, tabledata)
-                log.debug(file+'导入完成')
+                log.debug(file + '导入完成')
             except Exception as e:
                 log.error(e)
-                log.error(file+'未导入')
+                log.error(file + '未导入')
 
     def analysis(self, file, line=0, sheetname=None):
+        '''
+        Recursively traverse all the sheets of excel and return the DataFrame
+        :param file: list, file path
+        :param line: int, the start line
+        :param sheetname: str, sheetname
+        :return: None
+        '''
         a = pd.read_excel(file, sheetname=sheetname,
                           header=line)
         for k, v in a.items():
@@ -43,6 +53,6 @@ class ExcelDataStorage(DataStorageBase):
                 line += 1
                 self.analysis(file, line, [k])
             elif not v.empty:
-                v = pd.DataFrame(v.values, columns = [str(i).strip() for i in v.columns])
+                v = pd.DataFrame(v.values, columns=[str(i).strip() for i in v.columns])
                 k = k.strip()
-                self.data.append((k,v))
+                self.data.append((k, v))
